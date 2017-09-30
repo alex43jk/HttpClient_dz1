74364802eea230488ffadd20f1bd9b915d0e979d
Васильев Александр <leonbkone@yandex.ru>
2017-09-30 14:43:21 +0500

    Add files via upload

---
+++ HTTP_Parser/HttpParser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace HTTP_Parser
+{
+    public class HttpParser
+    {
+        List<Photo> photos;
+
+        public async void GetPhotos(string url)
+        {
+            photos = await GetPhotoList(url);
+        }
+
+        public async Task<List<Photo>> GetPhotoList(string url)
+        {
+            var result = new List<Photo>();
+            var httpClient = new HttpClient(new HttpClientHandler())
+            {
+                BaseAddress = new Uri(url)
+            };
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var resp = await httpClient.GetAsync(url).ConfigureAwait(false);
+
+            if (resp.IsSuccessStatusCode)
+            {
+                var content = resp.Content;
+                string task = await content.ReadAsStringAsync().ConfigureAwait(false);
+
+                var test = JsonConvert.DeserializeObject<Root>(task);
+                return test.Response.Items;
+            }
+            return new List<Photo>();
+        }
+
+        public class Root
+        {
+            public Response Response { get; set; }
+        }
+
+        public class Response
+        {
+            public int Count { get; set; }
+            public List<Photo> Items { get; set; }
+        }
+
+        public class Photo
+        {
+            public string Photo_75 { get; set; }
+            public string Photo_130 { get; set; }
+            public string Photo_604 { get; set; }
+            public double Lat { get; set; }
+            public double Long { get; set; }
+        }
+    }
+}
---
+++ HTTP_Parser/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HTTP_Parser
+{
+    [Activity(Label = "HTTP_Parser", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button parser;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView (Resource.Layout.Main);
+
+            var latitude = 46.7404283551898;
+            var longitude = 28.8811472434254;
+
+            string request = "https://api.vk.com/method/photos.search?lat=";
+            string url = request + latitude + "&long=" + longitude + "&count=150&radius=500&v=5.67";
+
+            parser = FindViewById<Button>(Resource.Id.button1);
+            parser.Click += delegate
+            {
+                HttpParser http = new HttpParser();
+                http.GetPhotos(url);
+            };
+        }
+    }
+}
+
